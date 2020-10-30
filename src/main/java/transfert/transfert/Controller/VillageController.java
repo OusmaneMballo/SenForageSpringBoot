702faa9a28bdac3a08e1736361a235f5b72108be
OusmaneMballo <ousmanemballo576@gmail.com>
2020-10-30 02:07:47 +0000

    Ajout de village

--- src/main/java/transfert/transfert/Controller/VillageController.java
+++ src/main/java/transfert/transfert/Controller/VillageController.java
@@ -2,7 +2,10 @@ package transfert.transfert.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import transfert.transfert.DAO.VillageRepository;
 import transfert.transfert.Model.Village;
@@ -19,5 +22,17 @@ public class VillageController {
         village.setNom("Mbodjene");
         villageRepos.save(village);*/
         return "village/index";
+    }
+
+    @PostMapping("/ajout")
+    public String add(@Validated Village village, BindingResult result){
+        try{
+                if (!result.hasErrors()){
+                    villageRepos.save(village);
+                }
+        }catch (Exception exception){
+            exception.getStackTrace();
+        }
+        return "redirect:/village/home";
     }
 }
