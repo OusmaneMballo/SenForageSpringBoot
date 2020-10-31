108183fbaaf87c4e6797182556267f985e1fbd7c
OusmaneMballo <ousmanemballo576@gmail.com>
2020-10-31 02:04:29 +0000

    Fonction add client

--- src/main/java/transfert/transfert/Controller/ClientController.java
+++ src/main/java/transfert/transfert/Controller/ClientController.java
@@ -3,10 +3,14 @@ package transfert.transfert.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import transfert.transfert.DAO.ClientRepository;
 import transfert.transfert.DAO.VillageRepository;
+import transfert.transfert.Model.Client;
 
 @Controller
 @RequestMapping("/client")
@@ -24,5 +28,19 @@ public class ClientController {
         model.addAttribute("villages", villageRepos.findAll());
 
         return "client/index";
+    }
+
+    @PostMapping("/ajout")
+    public String add(@Validated Client client, int id){
+
+        try{
+            /*if(!result.hasErrors()){
+
+            }*/
+            clientRepos.save(client);
+        }catch(Exception exception){
+            exception.getStackTrace();
+        }
+        return "redirect:/client/home";
     }
 }
